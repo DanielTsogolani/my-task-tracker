0307062dbdc18244bcb079b1b6bc5a99825d5111
DanielTsogolani <tsogolanidaniel23@gmail.com>
2022-11-28 18:16:18 +0200

    Refactor

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,16 @@ import Button from './Button'
 
 
 const Header = ({ title }) => {
+    const onClick = () => {
+        console.log('Click')
+    }
+
+
   return (
     <header className='header'>
         <h1>{title}</h1>
-        <Button />
+        <Button color='green' text='Add' onClick={onClick} />
+         
     </header>
   )
 }
